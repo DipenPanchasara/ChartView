6936bb7ca2367a21e3f749aa5a614953a89cb3d8
Dipen Panchasara <imediaimac@Dipens-iMac.local>
2015-05-26 19:11:07 +0530

    - Added Smoothness, curves between two connecting points.

--- DChartView/DChartView/DPlotChart.m
+++ DChartView/DChartView/DPlotChart.m
@@ -283,7 +283,7 @@
                 NSCalendar* calendar = [NSCalendar currentCalendar];
                 NSDateComponents* components = [calendar components:NSMonthCalendarUnit|NSDayCalendarUnit fromDate:dateFromString];
                                           
-                [[NSString stringWithFormat:@"%d-%@",[components day],[self getMonthNameByNumber:[components month]]] drawAtPoint:CGPointMake(i-10, kTopMargin + self.chartHeight + 10) withFont:kStatesticFont];
+                [[NSString stringWithFormat:@"%ld-%@",(long)[components day],[self getMonthNameByNumber:[components month]]] drawAtPoint:CGPointMake(i-10, kTopMargin + self.chartHeight + 10) withFont:kStatesticFont];
                 CGContextStrokePath(context);
             }
             
@@ -303,7 +303,7 @@
             NSCalendar* calendar = [NSCalendar currentCalendar];
             NSDateComponents* components = [calendar components:NSMonthCalendarUnit|NSDayCalendarUnit fromDate:dateFromString];
             
-            [[NSString stringWithFormat:@"%d-%@",[components day],[self getMonthNameByNumber:[components month]]] drawAtPoint:CGPointMake(kTopMargin + self.chartWidth-10, kTopMargin + self.chartHeight + 10) withFont:kStatesticFont];
+            [[NSString stringWithFormat:@"%ld-%@",(long)[components day],[self getMonthNameByNumber:[components month]]] drawAtPoint:CGPointMake(kTopMargin + self.chartWidth-10, kTopMargin + self.chartHeight + 10) withFont:kStatesticFont];
         
             CGContextStrokePath(context);
             
@@ -358,7 +358,15 @@
                     
                     // Add Point & Draw
                     CGContextMoveToPoint(context, self.prevPoint.x, self.prevPoint.y);
-                    CGContextAddLineToPoint(context, self.curPoint.x, self.curPoint.y);
+//                    CGContextAddLineToPoint(context, self.curPoint.x, self.curPoint.y);
+                    
+                    // *** Calculate curve between two points for smoothness ***
+                    CGPoint midPoint = midPointForPoints(self.prevPoint, self.curPoint);
+                    CGPoint cp1 = controlPointForPoints(midPoint, self.prevPoint);
+                    CGContextAddQuadCurveToPoint(context, cp1.x, cp1.y, midPoint.x, midPoint.y);
+                    CGPoint cp2 = controlPointForPoints(midPoint, self.curPoint);
+                    CGContextAddQuadCurveToPoint(context, cp2.x, cp2.y, self.curPoint.x, self.curPoint.y);
+
                     CGContextStrokePath(context);
 
                     // Set Line Color & Width
@@ -508,6 +516,50 @@
     
     isMovement = NO;
     [self setNeedsDisplay];
+}
+
++ (UIBezierPath *)quadCurvedPathWithPoints:(NSArray *)points
+{
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    
+    NSValue *value = points[0];
+    CGPoint p1 = [value CGPointValue];
+    [path moveToPoint:p1];
+    
+    if (points.count == 2) {
+        value = points[1];
+        CGPoint p2 = [value CGPointValue];
+        [path addLineToPoint:p2];
+        return path;
+    }
+    
+    for (NSUInteger i = 1; i < points.count; i++) {
+        value = points[i];
+        CGPoint p2 = [value CGPointValue];
+        
+        CGPoint midPoint = midPointForPoints(p1, p2);
+        [path addQuadCurveToPoint:midPoint controlPoint:controlPointForPoints(midPoint, p1)];
+        [path addQuadCurveToPoint:p2 controlPoint:controlPointForPoints(midPoint, p2)];
+        
+        p1 = p2;
+    }
+    return path;
+}
+
+static CGPoint midPointForPoints(CGPoint p1, CGPoint p2) {
+    return CGPointMake((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+}
+
+static CGPoint controlPointForPoints(CGPoint p1, CGPoint p2) {
+    CGPoint controlPoint = midPointForPoints(p1, p2);
+    CGFloat diffY = abs(p2.y - controlPoint.y);
+    
+    if (p1.y < p2.y)
+        controlPoint.y += diffY;
+    else if (p1.y > p2.y)
+        controlPoint.y -= diffY;
+    
+    return controlPoint;
 }
 
 
